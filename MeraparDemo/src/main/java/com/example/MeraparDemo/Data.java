4df0cc0535785ed1bb2dcb35385a47e0fb95b544
DyingLightHD <mcmuffin784@gmail.com>
2018-01-07 23:16:35 +0100

    MeraparDemo

---
+++ MeraparDemo/src/main/java/com/example/MeraparDemo/Data.java
@@ -0,0 +1,64 @@
+package com.example.MeraparDemo;
+
+import java.util.ArrayList;
+
+public class Data {
+
+    private ArrayList<Integer> scoreList = new ArrayList<>();
+
+    private String URL;
+    private Character firstCharOfString;
+
+    //Merapar Meta Data
+    private String analyseDate;
+    private String firstPost;
+    private String lastPost;
+
+    private int totalPosts;
+    private int totalAcceptedPosts;
+    private int avgScore;
+
+
+    public String getURL() {
+        return URL;
+    }
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    public Character getFirstCharOfString() {
+        return firstCharOfString;
+    }
+
+    public void setFirstCharOfString(Character firstCharOfString) {
+        this.firstCharOfString = firstCharOfString;
+    }
+
+    public void setTotalPosts(int totalPosts) {
+        this.totalPosts += totalPosts;
+    }
+    public int getTotalPosts() {
+        return totalPosts;
+    }
+
+    public int getAvgScore() {
+        return avgScore;
+    }
+    public void setAvgScore(int avgScore) {
+        this.avgScore = avgScore;
+    }
+
+    public void AddScore(Integer s){
+        scoreList.add(s);
+    }
+
+    public void GetAverageScore(){
+        int total = 0;
+
+        for(Integer i : scoreList){
+            total += i;
+        }
+
+        avgScore = total / scoreList.size();
+    }
+}
---
+++ MeraparDemo/src/main/java/com/example/MeraparDemo/DataController.java
@@ -0,0 +1,84 @@
+package com.example.MeraparDemo;
+
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ListIterator;
+
+@Component
+public class DataController {
+
+
+    public void analyzeData(Data data) {
+
+        String urlString = data.getURL();
+        Boolean getNextValue = false;
+        String getNextValueString = "";
+
+        try {
+            URL url = new URL(urlString);
+
+            URLConnection urlConnection = url.openConnection();
+
+            HttpURLConnection connection = null;
+
+            if (urlConnection instanceof HttpURLConnection) {
+                connection = (HttpURLConnection) urlConnection;
+            } else {
+                System.out.println("Please enter an HTTP URL.");
+                return;
+            }
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String current;
+
+            while ((current = in.readLine()) != null) {
+                urlString += current;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] words = urlString.split("=|\\s");
+
+        for (int i = 0; i < words.length; i++) {
+
+            words[i] = words[i].replace("\"","");
+
+            if (getNextValue)
+                switch (getNextValueString) {
+                    case "Score":
+                        int x = Integer.parseInt(words[i]);
+                        //System.out.println(x);
+                        data.AddScore(x);
+                        getNextValue = false;
+                        break;
+                }
+
+            switch (words[i]) {
+                case "PostTypeId":
+                    data.setTotalPosts(1);
+                    break;
+                case "Score":
+                    getNextValue = true;
+                    getNextValueString = "Score";
+                    break;
+            }
+        }
+
+        data.GetAverageScore();
+        System.out.println(String.valueOf(data.getTotalPosts()));
+        data.setFirstCharOfString(data.getURL().charAt(0));
+    }
+}
